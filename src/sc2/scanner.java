266972e3f9cbb37fde90f15c02c8357a80142c3b
pcjha <pc.jha215@gmail.com>
2023-11-08 00:21:38 +0530

    Main

--- src/sc2/scanner.java
+++ src/sc2/scanner.java
@@ -9,11 +9,17 @@ public class scanner {
 		
 		Scanner input = new Scanner(System.in);
 		
-		System.out.println("Please enteryour name: ");
+		System.out.print("Please enteryour name: ");
 		
 		String name = input.nextLine();
 		
-				System.out.println("You enter your name as : " +name);
+		System.out.println("You enter your name as : " +name);
+		
+		System.out.println("Please enteryour age: ");
+		
+		String age = input.nextLine();
+				
+				System.out.println("You enter your ag as : " +age);
 				input.close();
 	}
 
